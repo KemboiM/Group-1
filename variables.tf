de157211198e5730fe51a63437d202ed08683547
KemboiM <millsortrobins@gmail.com>
2026-04-28 18:54:32 +0300

    docs: add inline comments to variables.tf for clarity

--- variables.tf
+++ variables.tf
@@ -1,4 +1,20 @@
+# The name of the Azure Resource Group where all resources will be deployed
 variable "resource_group_name" {
+  type        = string
+  description = "Name of the Azure Resource Group"
+}
+
+# The Azure region/location where resources will be provisioned (e.g., centralus)
+variable "location" {
+  type        = string
+  description = "Azure region for resource deployment"
+}
+
+# The administrator username used to SSH into the virtual machine
+variable "admin_username" {
+  type        = string
+  description = "Admin username for the virtual machine"
+}variable "resource_group_name" {
   type = string
 }
 
